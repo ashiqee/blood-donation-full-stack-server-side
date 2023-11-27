2c711ae9033b82a0cce804d9fb3a8aafc2631b03
ashiq73 <ashiq.buet73@gmail.com>
2023-11-27 14:09:08 +0600

    admin user management funtional

--- src/models/user.js
+++ src/models/user.js
@@ -1,44 +1,50 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        require: true,
-    },
-    email: {
-        type: String,
-        require: true,
-    },
-    profileImg
-        : {
-        type: String,
-        require: true,
-    },
-    coverImg
-        : {
-        type: String,
-
-    },
-    blood: {
-        type: String,
-        require: true,
-    },
-    districts: {
-        type: String,
-        require: true,
-    },
-    upuzilla: {
-        type: String,
-        require: true,
-    },
-    role: {
-        type: String,
-        enum: ['Admin', 'Donor', 'Volunteer', 'Guest']
-    },
-    status: {
-        type: String,
-        enum: ["Active", "Blocked"]
-    },
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  profileImg: {
+    type: String,
+    require: true,
+  },
+  coverImg: {
+    type: String,
+  },
+  blood: {
+    type: String,
+    require: true,
+  },
+  districts: {
+    type: String,
+    require: true,
+  },
+  upuzilla: {
+    type: String,
+    require: true,
+  },
+  role: {
+    type: String,
+    enum: ["Admin", "Donor", "Volunteer", "Guest"],
+  },
+  roleStatus: {
+    type: String,
+    enum: [
+      "no request",
+      "pending request for donor",
+      "pending request for volunteer",
+      "Approved",
+    ],
+  },
+  status: {
+    type: String,
+    enum: ["Active", "Blocked"],
+  },
 });
 
 const user = model("users", userSchema);
--- src/routes/users/user.js
+++ src/routes/users/user.js
@@ -6,81 +6,159 @@ var router = express.Router();
 //post user data
 
 router.post("/user", async (req, res) => {
-    const user = req.body;
-    try {
+  const user = req.body;
+  console.log(user);
 
-        const result = await users.create(user);
-        res.send(result);
-    } catch (err) {
-        res.status(500).send(err.message)
-    }
+  const result = await users.create(user);
+  res.send(result);
+
+  //   try {
+  //     const result = await users.create(user);
+  //     res.send(result);
+  //   } catch (err) {
+  //     res.status(500).send(err.message);
+  //   }
 });
 
+// get all user data
 
-// get all user data 
-
-router.get('/users', async (req, res) => {
-    const cursor = await users.find().select({
-        _v: 0,
-
-    });
-    res.send(cursor)
-})
+router.get("/users", async (req, res) => {
+  const cursor = await users.find().select({
+    _v: 0,
+  });
+  res.send(cursor);
+});
 
 //get single user data
 
-router.get('/user/:email', async (req, res) => {
+router.get("/user/:email", async (req, res) => {
+  const result = await users.findOne({ email: req.params.email });
+  res.send(result);
+});
 
-    const result = await users.findOne({ email: req.params.email })
-    res.send(result)
-})
+//user donor request
+
+router.patch("/user/donorReq/:id", async (req, res) => {
+  const userDonorReq = req.body;
+  console.log(userDonorReq);
+  const result = await users.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        roleStatus: userDonorReq.roleStatus,
+      },
+    }
+  );
+  console.log(result);
+  res.send(result);
+});
+//user Volunteer request
+
+router.patch("/user/volunteerReq/:id", async (req, res) => {
+  const userDonorReq = req.body;
+  console.log(userDonorReq);
+  const result = await users.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        roleStatus: userDonorReq.roleStatus,
+      },
+    }
+  );
+  console.log(result);
+  res.send(result);
+});
+
+//admin section
+
+router.get("/user/admin/:email", async (req, res) => {
+  const user = await users.findOne({ email: req.params.email });
+  let admin = false;
+  if (user) {
+    admin = user?.role === "Admin";
+  }
+  console.log(admin);
+  res.send({ admin });
+});
+
+//admin
+// user request donor aprroval
+router.patch("/user/admin/donorReq/:id", async (req, res) => {
+  const result = await users.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        role: "Donor",
+        roleStatus: "Approved",
+      },
+    }
+  );
+  res.send(result);
+});
+// user request Volunteer aprroval
+router.patch("/user/admin/volunteerReq/:id", async (req, res) => {
+  const result = await users.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        role: "Volunteer",
+        roleStatus: "Approved",
+      },
+    }
+  );
+  res.send(result);
+});
 
 //patch user role update
-router.patch('/user/admin/:id', async (req, res) => {
-    const roleData = req.body;
+router.patch("/user/admin/:id", async (req, res) => {
+  const roleData = req.body;
 
-    const result = await users.updateOne({ _id: req.params.id }, {
-        $set: {
-            role: roleData.role
-        },
-    },
-    )
-    res.send(result)
+  const result = await users.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        role: roleData.role,
+        roleStatus: roleData.roleStatus,
+      },
+    }
+  );
+  res.send(result);
+});
 
-})
 //patch user status update
-router.patch('/user/status/:id', async (req, res) => {
-    const currentStatus = req.body;
-    console.log(currentStatus.status);
-    const result = await users.updateOne({ _id: req.params.id }, {
-        $set: {
-            status: currentStatus.status
-        },
-    },
-    )
-    res.send(result)
-
-})
-
+router.patch("/user/status/:id", async (req, res) => {
+  const currentStatus = req.body;
+  console.log(currentStatus.status);
+  const result = await users.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: currentStatus.status,
+      },
+    }
+  );
+  res.send(result);
+});
 
 // user profile update
 
-router.patch('/user/updateProfile/:id', async (req, res) => {
-    const updateData = req.body;
+router.patch("/user/updateProfile/:id", async (req, res) => {
+  const updateData = req.body;
 
-    const result = await users.findByIdAndUpdate({ _id: req.params.id }, {
-        $set: {
-            name: updateData?.name,
-            profileImg: updateData?.profileImg,
-            coverImg: updateData?.coverImg,
-            blood: updateData?.blood,
-            districts: updateData?.districts,
-            upuzilla: updateData?.upuzilla,
-
-        }
-    })
-    res.send(result)
-
-})
+  const result = await users.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        name: updateData?.name,
+        profileImg: updateData?.profileImg,
+        coverImg: updateData?.coverImg,
+        blood: updateData?.blood,
+        districts: updateData?.districts,
+        upuzilla: updateData?.upuzilla,
+      },
+    }
+  );
+  res.send(result);
+});
 
 module.exports = router;
